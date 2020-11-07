58b62a14b48da44570f807fc8c1b7d0150ecc797
bayusahidp <sahid_pratomo@yahoo.com>
2020-11-07 17:40:46 +0700

    perbarui fungsi curl

--- fitur/moota/program_moota.php
+++ fitur/moota/program_moota.php
@@ -4,265 +4,73 @@ $baseURLMoota = "https://app.moota.co/";
 $apiTokenMoota = "";
 
 function read_pofile() {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/profile',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+    $urlAPI = "https://app.moota.co/api/v1/profile";
+    return curlAPI($urlAPI);
 }
 
 function read_saldo() {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/balance',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = "https://app.moota.co/api/v1/balance";
+  return curlAPI($urlAPI);
 }
 
 function daftar_akunBank() {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = "https://app.moota.co/api/v1/bank";
+  return curlAPI($urlAPI);
 }
 
 function detail_akunBank($bankId) {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank/'.$bankId,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = 'https://app.moota.co/api/v1/bank/'.$bankId;
+  return curlAPI($urlAPI);
 }
 
 function mutasi_bulanIni($bankId) {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/';
+  return curlAPI($urlAPI);
 }
 
 function mutasi_terakhir($bankId, $jumlah) {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/recent/'.$jumlah,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/recent/'.$jumlah;
+  return curlAPI($urlAPI);
 }
 
 function mutasi_nominal($bankId, $amount) {
-    global $apiTokenMoota;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/search/'.$amount,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+  $urlAPI = 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/search/'.$amount;
+  return curlAPI($urlAPI);
 }
 
 function mutasi_deskripsi($bankId, $description) {
-    global $apiTokenMoota;
-    $curl = curl_init();
+  $urlAPI = 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/search/description/'.$description;
+  return curlAPI($urlAPI);
+}
 
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://app.moota.co/api/v1/bank/'.$bankId.'/mutation/search/description/'.$description,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        'Accept: application/json',
-        'Authorization: Bearer '.$apiTokenMoota
-      ],
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-      echo $response;
-    }
-    
-    $result = json_decode($response, true);
-    return $result;
+function curlAPI($urlAPI) {
+  global $apiTokenMoota;
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $urlAPI,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+      'Accept: application/json',
+      'Authorization: Bearer '.$apiTokenMoota
+    ],
+  ));
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  
+  curl_close($curl);
+  
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+  
+  $result = json_decode($response, true);
+  return $result;
 }
